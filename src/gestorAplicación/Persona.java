7ed3461646a20a861f4edc8e3b457b310ff50e95
Jhorman-Shair <jhramirezhe@unal.edu.co>
2024-03-27 11:14:56 -0500

    Creación de algunos atributos
    Algunas clases ya tienen atributos pero necesitan revisión, ya que hay algunas aun en búsqueda como tipocarrito, que esta en problemas y tipopasillo

--- src/gestorAplicación/Persona.java
+++ src/gestorAplicación/Persona.java
@@ -1,5 +1,11 @@
 package gestorAplicación;
 
 public class Persona {
-
+	private tipoPersona tipo;
+	private String nombre;
+	private int id;
+	private float dinero;
+	private int edad;
+	private Tienda tienda;
+	private String genero;
 }
