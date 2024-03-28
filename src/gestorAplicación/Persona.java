99fcb660035090145d0ce5535a0a8881ae447458
Juanes6767 <jupinedahe@unal.edu.co>
2024-03-28 09:11:51 -0500

    get/set Persdona
    Estos get/set están muy sujetos a cambios

--- src/gestorAplicación/Persona.java
+++ src/gestorAplicación/Persona.java
@@ -8,4 +8,60 @@ public class Persona {
 	private int edad;
 	private Tienda tienda;
 	private String genero;
+	
+	public tipoPersona getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(tipoPersona tipo) {
+		this.tipo=tipo;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public float getDinero() {
+		return dinero;
+	}
+	
+	public void setDinero(float dinero) {
+		this.dinero=dinero;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	
+	public void setEdad(int edad) {
+		this.edad=edad;
+	}
+	
+	public Tienda getTienda() {
+		return tienda;
+	}
+	
+	public void setTienda(Tienda tienda) {
+		this.tienda=tienda;
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
+	
+	public void setGenero(String genero) {
+		this.genero=genero;
+	}
 }
