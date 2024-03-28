940a8fd67e108f19c309da586db5aa3d0f78262d
Juanes6767 <jupinedahe@unal.edu.co>
2024-03-28 09:11:36 -0500

    get/set inventario
    Estos get/set están muy sujetos a cambios

--- src/gestorAplicación/Inventario.java
+++ src/gestorAplicación/Inventario.java
@@ -10,4 +10,28 @@ public class Inventario {
 	public String solicitarInventario() {
 		return "Nada por ahora";
 	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public ArrayList<ArrayList<Producto>> getProductos() {
+		return productos;
+	}
+	
+	public void setProductos(ArrayList<ArrayList<Producto>> productos) {
+		this.productos=productos;
+	}
+	
+	public Tienda getTienda() {
+		return tienda;
+	}
+	
+	public void setTienda(Tienda tienda) {
+		this.tienda=tienda;
+	}
 }
