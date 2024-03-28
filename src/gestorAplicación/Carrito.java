8f17dde0d919f968822fb7444880a260ad0e156d
Juanes6767 <jupinedahe@unal.edu.co>
2024-03-28 09:11:14 -0500

    Métodos get/set Carrito
    Estos get/set están muy sujetos a cambios

--- src/gestorAplicación/Carrito.java
+++ src/gestorAplicación/Carrito.java
@@ -6,4 +6,28 @@ public class Carrito {
 	private ArrayList <Producto> productos=new ArrayList<Producto>();
 	private Persona cliente;
 	private Tienda tienda;
+	
+	public ArrayList<Producto> getProductos() {
+		return productos;
+	}
+	
+	public void setProductos(ArrayList<Producto> productos) {
+		this.productos=productos;
+	}
+	
+	public Persona getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Persona cliente) {
+		this.cliente=cliente;
+	}
+	
+	public Tienda getTienda() {
+		return tienda;
+	}
+	
+	public void setTienda(Tienda tienda) {
+		this.tienda=tienda;
+	}
 }
