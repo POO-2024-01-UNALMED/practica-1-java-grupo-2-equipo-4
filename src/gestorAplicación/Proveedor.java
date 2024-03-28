0d1c1eac5227fa1fa288f75c68b7817a8b0a82e4
Juanes6767 <jupinedahe@unal.edu.co>
2024-03-28 09:12:01 -0500

    get/set
    Estos get/set están muy sujetos a cambios

--- src/gestorAplicación/Proveedor.java
+++ src/gestorAplicación/Proveedor.java
@@ -8,4 +8,44 @@ public class Proveedor {
 	private categoria tipo;
 	private Tienda tienda;
 	private Inventario inventario;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public ArrayList<Producto> getEntrega() {
+		return entrega;
+	}
+	
+	public void setEntrega(ArrayList<Producto> entrega) {
+		this.entrega=entrega;
+	}
+	
+	public categoria getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(categoria tipo) {
+		this.tipo=tipo;
+	}
+	
+	public Tienda getTienda() {
+		return tienda;
+	}
+	
+	public void setTienda(Tienda tienda) {
+		this.tienda=tienda;
+	}
+	
+	public Inventario getInventario() {
+		return inventario;
+	}
+	
+	public void setInventario(Inventario inventario) {
+		this.inventario=inventario;
+	}
 }
