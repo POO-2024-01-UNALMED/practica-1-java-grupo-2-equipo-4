638dad9b6cde53210321e972218235e7bf82e050
Juanes6767 <jupinedahe@unal.edu.co>
2024-03-28 09:10:38 -0500

    Métodos get/set Caja
    Estos get/set están muy sujetos a cambios

--- src/gestorAplicación/Caja.java
+++ src/gestorAplicación/Caja.java
@@ -7,4 +7,53 @@ public class Caja {
 	private TipoCaja tipo;
 	private Tienda tienda;
 	private String descuento;
+	
+	public Empleado getEmpleado() {
+		return empleado;
+	}
+	
+	public void setEmpleado(Empleado empleado) {
+		this.empleado=empleado;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	
+	public int getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(int estado) {
+		this.estado=estado;
+	}
+	
+	public Empleado getTipo() {
+		return empleado;
+	}
+	
+	public void setTipo(TipoCaja tipo) {
+		this.tipo=tipo;
+	}
+	
+	public Tienda setTienda() {
+		return tienda;
+	}
+	
+	public void setTienda(Tienda tienda) {
+		this.tienda=tienda;
+	}
+	
+	public String setDescuento() {
+		return descuento;
+	}
+	
+	public void setDescuento(String descuento) {
+		this.descuento=descuento;		
+		
+	}
 }
