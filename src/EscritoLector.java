01193683ce9678e5e8c72ec50c35566c473df272
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-03-30 15:59:40 -0500

    Create EscritoLector.java

---
+++ src/EscritoLector.java
@@ -0,0 +1,12 @@
+import java.io.File; 
+import java.io.FileInputStream; 
+import java.io.FileOutputStream; 
+import java.io.ObjectInputStream; 
+import java.io.ObjectOutputStream;
+
+public class EscritorLector {
+  static File archivo = new File("");
+
+  public static void main(String[] args) {  
+  }
+}
