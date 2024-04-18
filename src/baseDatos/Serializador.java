2729713e988b7c40535eeb07b70d9097b56547d9
Juanes6767 <jupinedahe@unal.edu.co>
2024-04-18 16:02:06 -0500

    Deserializador.java y serializador.java
    Clases para leer y guardar archivos

---
+++ src/baseDatos/Serializador.java
@@ -0,0 +1,17 @@
+package baseDatos;
+
+import java.io.File; 
+import java.io.FileInputStream; 
+import java.io.FileOutputStream; 
+import java.io.ObjectInputStream; 
+import java.io.ObjectOutputStream; 
+import java.io.IOException; 
+import java.io.FileNotFoundException;
+
+public class Serializador {
+	private static File rutaTemp = new File("src\\baseDatos\\temp");
+	
+	public static void deserealizar(/*Parametro*/) {
+		
+	}
+}
