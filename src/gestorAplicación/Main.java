0e8b83140c4d24ceb45f60bd5c62d52d985bc90c
juandiegoordonezl <159456074+juandiegoordonezl@users.noreply.github.com>
2024-05-14 21:18:15 -0500

    Avance Caso 1 y creacion de una clase provicional
    -Se creo la clase CodigoViejo por motivos de saturación del main en este se encuentra el código que se encontraba en la clase main
    
    -Se hizo avances en la interacción del usuario en el caso 1

--- src/gestorAplicación/Main.java
+++ src/gestorAplicación/Main.java
@@ -73,7 +73,7 @@ public class Main {
 		
 	}
 	
-	public static void main(String[] args){
+	public static void main(String[] args){		
 //-----------------------------------------------------------------------------------------------------------
 	do{
 		boolean boleano=false;
@@ -104,6 +104,77 @@ public class Main {
 		}
 		switch(decision){
 		case 1:
+			print("Elige la categoria que estas buscando ");
+			int enumerado = 1;
+			for(Categoria tipo:Categoria.values()) {
+					print(enumerado +"."+tipo );
+					enumerado++;
+				
+			}
+			int desicionCategoria = sc.nextInt();
+			switch(desicionCategoria){
+			case 1:
+				print("Estas tiendas tienen tu categoria deseada en cual deseas comprar:");
+				enumerado=1;
+				for(Tienda alimento:BaseDatos.buscarTienda(Categoria.ALIMENTO)) {
+					print(enumerado+"."+alimento.getNombre());
+					enumerado++;
+				} 
+				
+				break;
+			
+			case 2:
+				print("Estas tiendas tienen tu categoria deseada en cual deseas comprar:");
+				enumerado=1;
+				for(Tienda alimento:BaseDatos.buscarTienda(Categoria.BEBIDA)) {
+					print(enumerado+"."+alimento.getNombre());
+					enumerado++;
+				} 
+				
+				break;
+				
+			case 3:
+				print("Estas tiendas tienen tu categoria deseada en cual deseas comprar:");
+				enumerado=1;
+				for(Tienda alimento:BaseDatos.buscarTienda(Categoria.LIMPIEZA)) {
+					print(enumerado+"."+alimento.getNombre());
+					enumerado++;
+				} 
+			
+				break;
+				
+			case 4:
+				print("Estas tiendas tienen tu categoria deseada en cual deseas comprar:");
+				enumerado=1;
+				for(Tienda alimento:BaseDatos.buscarTienda(Categoria.PERSONAL)) {
+					print(enumerado+"."+alimento.getNombre());
+					enumerado++;
+				} 
+			
+				break;
+				
+			case 5:
+				print("Estas tiendas tienen tu categoria deseada en cual deseas comprar:");
+				enumerado=1;
+				for(Tienda alimento:BaseDatos.buscarTienda(Categoria.HOGAR)) {
+					print(enumerado+"."+alimento.getNombre());
+					enumerado++;
+				} 
+				
+				break;
+			case 6:
+				print("Estas tiendas tienen tu categoria deseada en cual deseas comprar:");
+				enumerado=1;
+				for(Tienda alimento:BaseDatos.buscarTienda(Categoria.ELECTRONICO)) {
+					print(enumerado+"."+alimento.getNombre());
+					enumerado++;
+				} 
+				
+				break;
+				
+			}
+			
+			break;
 			
 		case 2:
 			
@@ -115,121 +186,6 @@ public class Main {
 		
 		}
 	 } while (decision!=6);
-//-----------------------------------------------------------------------------------------------------------
-		
-	
-		
-		int n=1; // este contador lo pueden usar a gusto,reiniciar a pls
-		BaseDatos baseDatos1=new BaseDatos();
-		
-		//crear pasillos de tiendas
-		Persona persona1=new Persona("pacho",1035,34,"masculino");
-		Tienda tienda1= new Tienda("b101",persona1,"pacho's","cra20",10.000000,"abierto");
-		crearPasillos(tienda1);
-		for(Proveedor i:tienda1.getProveedores()) {
-		System.out.println(n+"."+i);
-		n++;
-		}
-		n=1;	
-		
-		Tienda tienda2= new Tienda("a201",persona1,"pachos2","cra20",10.000000,"abierto");
-		tienda1.disponibilidadProductos();
-		tienda2.disponibilidadProductos();
-		tienda1.mostrarPasillos();
-		tienda2.mostrarPasillos();
-		
-		System.out.println(tienda1.getPasillos());
-		System.out.println(tienda1.getPasillos().get(0).getCategoria() );
-		System.out.println(tienda1.getPasillos().get(1).getCategoria() );
-		
-		//busca tiendas devuelve tiendas disponibles 
-		System.out.println("Buscando tiendas...Categorias disponibles:");
-		for(Categoria cat:Categoria.values()) {
-			System.out.println(n +"."+cat );
-			n++;
-		}
-		n=1;
-		
-		System.out.println("Seleccione una Categoria:" );
-		 int x1=sc.nextInt();
-		
-		System.out.println("Tiendas segun su categoria:" );
-		System.out.println(baseDatos1.buscarTienda(Categoria.values()[x1]));
-		
-		System.out.println("Seleccione una tienda" );
-		int x3 = sc.nextInt();
-		ArrayList<Tienda>lista = baseDatos1.buscarTienda(Categoria.values()[x1]);
-		Tienda tiendaSelec=lista.get(x3);
-		
-		System.out.println("productos categoria:"+Categoria.values()[x1]);		
-		baseDatos1.buscarProducto(tiendaSelec, n);
-		//-----------------------------------------------------------------------------------------------------
-		
-		//Creando constructor de persona (WIP)
-		System.out.println("Introduzca su nombre: ");
-		String nombre = sc.nextLine();
-		System.out.println("Introduzca su ## de identificacion: ");
-		int id =sc.nextInt();
-		System.out.println("Introduzca su edad: ");
-		int edad = sc.nextInt();
-		
-		
-		//Creando arraylist de las cajas buenas y agregandolas (WIP)
-		ArrayList<Caja> cajas=new ArrayList<Caja>();
-		tienda1.cajasDisponibles(cajas);
-		if (cajas.size()==0) {
-			int contador=0;
-			int decision=1;
-			while(cajas.size()==0 & decision==1) {
-			System.out.println("Parece que no hay cajas disponibles, ¿que desea hacer?");
-			System.out.println("1. Esperar que haya una caja disponible");
-			System.out.println("2. Dejar los productos e irse de la tienda");
-			decision = sc.nextInt();
-			if (decision==1) {
-				contador++;
-				//Poner aca que hacer si el cliente espera y con el contador dependiendo de cuanto espera
-			}
-			if (decision==2) {
-				//Necesito metodo para devolver productos
-				break;
-			}
-			}
-		}
-		
-		for (Caja i:cajas) {
-			System.out.println(i.getNombre()+" Tipo "+i.getTipo());
-		}
-		System.out.println("Recuerde que las cajas rapidas son para 5 productos o menos");
-		boolean comprobacionNom=false;
-		boolean comprobacionPro=false;
-		String eleccion=null;
-		while (!comprobacionNom & !comprobacionPro) {
-		System.out.println("¿Cual caja desea escoger?:");
-		eleccion=sc.nextLine();
-		for (Caja i:cajas) {
-			if (i.getNombre()==eleccion) {
-				comprobacionNom=true;
-				if (i.getTipo()==TipoCaja.RAPIDA & i.getTienda().getCliente().getCarrito().getProductos().size()<=5) {
-						comprobacionPro=true;
-				}
-				if (i.getTipo()==TipoCaja.LENTA) {
-					comprobacionPro=true;
-				}
-			}	
-		}
-		if (comprobacionNom=false) {
-			System.out.println("Esa caja no existe");
-		}
-		
-		if (comprobacionPro=false) {
-			System.out.println("Esta escogiendo una caja rapida con mas de 5 productos");
-		}
-		}
-		
-		for (Caja p:cajas) {
-			if (p.getNombre()==eleccion) {
-				p.setCliente(p.getTienda().getCliente());
-			}
-		}
+
 	}
 }
