3814f521a31f63196cd86bef46bdde0f56522872
Jhorman-Shair <jhramirezhe@unal.edu.co>
2024-05-24 12:47:13 -0500

    Revert "Paquete uiMain y codigo reordenado (#10)"
    This reverts commit e81d795ab7e7c6de779724742a386fda88fcc7c5.

--- src/gestorAplicación/Main.java
+++ src/gestorAplicación/Main.java
@@ -1,63 +1,18 @@
-package uiMain;
+package gestorAplicación;
 import java.util.Scanner;
-
-import gestorAplicación.*;
+import java.util.ArrayList;
 
 public class Main {
 	static Scanner sc = new Scanner(System.in);
-
+	Tienda tiendaDefacto;
+	static long readLong(){
+		return sc.nextLong();
+	}
+	
 	public static void print(String p) {
 		System.out.println(p);
 	}
 	
-	static int [] numeros = {1,2,3,4,5,6};
-	static int decision;
-	
-	public static void main(String[] args){
-//-----------------------------------------------------------------------------------------------------------
-	do{
-		boolean boleano=false;
-		print("Bienvenido a My_Tiendita, que desea hacer?");
-		print("1.Mostrar productos disponibles segun categoria y tienda\n"
-				+ "2.Realizar listas de compras\n"
-				+ "3.Pagar recibos pendientes\n"
-				+ "4. \n"
-				+ "5.Personalizar y modificar tiendas\n"
-				+ "6.Terminar");
-		while (!boleano){
-			try{
-				decision=sc.nextInt();
-			}
-			catch(Exception e) {
-				print("Este no es un numero valido");
-				sc.nextLine();
-			}
-			for (int i:numeros){
-				 if (decision==i){
-				        boleano=true;
-				 }
-			}
-			if (boleano==false){
-				 print("El numero esta fuera del rango");
-				 continue;
-			}
-		}
-		switch(decision){
-		case 1:
-			mostrarProductos();
-			break;
-		case 2:
-			realizarListaCompra();
-		case 3:
-		
-		case 4:
-			
-		case 5:
-		
-		}
-	 } while (decision!=6);
-
-	}	
 	// -------- FUNCIONALIDAD 1 ----------------------------------------------------------------
 	public static void mostrarProductos(){
 		print("Elige la categoria que desea buscar: ");
@@ -234,4 +189,95 @@ public class Main {
 	}
 	
 	
+	static int [] numeros = {1,2,3,4,5,6};
+	static int decision;
+	
+	public static void crearPasillos(Tienda tienda) {
+		ArrayList <Categoria> categorias=new ArrayList<>();
+		System.out.println("cuantos pasillos desea crear?");
+		int x1=sc.nextInt();
+		
+		for(int i=1;i<=x1;i++) {
+			System.out.println("que categorias tendra el pasillo "+i+"?");
+			int n=1;
+			for(Categoria j:Categoria.values()) {
+				System.out.println(n+"."+j);
+				n++;
+			}
+			int x2=sc.nextInt();
+			sc.nextLine();
+			categorias.add(Categoria.values()[x2-1]);
+		}
+		
+		int n=1;
+		for(Categoria k:categorias) {
+			
+			Categoria cat=k;
+			System.out.println("nombre del pasillo "+n+" :");
+			String x2=sc.nextLine();
+			Pasillo pasillo=new Pasillo(x2,cat);
+			tienda.getPasillos().add(pasillo);
+			System.out.println("pasillo creado");
+			n++;
+		}	
+		
+		if (tienda.disponibilidadProductos()==false) {
+			System.out.println(tienda.getDueño() +"de la tienda"+tienda.getNombre()+ " sus pasillos estan vacios ");
+			System.out.println("desea llamar a un proveedor?\n1.Si\n2.No");
+			int x3=sc.nextInt();
+			
+			if(x3==1) {
+				tienda.llamarProveedor();
+			}
+			
+		}
+		
+		
+	}
+	
+	public static void main(String[] args){
+//-----------------------------------------------------------------------------------------------------------
+	do{
+		boolean boleano=false;
+		print("Bienvenido a My_Tiendita, que desea hacer?");
+		print("1.Mostrar productos disponibles segun categoria y tienda\n"
+				+ "2.Realizar listas de compras\n"
+				+ "3.Pagar recibos pendientes\n"
+				+ "4. \n"
+				+ "5.Personalizar y modificar tiendas\n"
+				+ "6.Terminar");
+		while (!boleano){
+			try{
+				decision=sc.nextInt();
+			}
+			catch(Exception e) {
+				print("Este no es un numero valido");
+				sc.nextLine();
+			}
+			for (int i:numeros){
+				 if (decision==i){
+				        boleano=true;
+				 }
+			}
+			if (boleano==false){
+				 print("El numero esta fuera del rango");
+				 continue;
+			}
+		}
+		switch(decision){
+		case 1:
+			mostrarProductos();
+			break;
+		case 2:
+			realizarListaCompra();
+		case 3:
+		
+		case 4:
+			
+		case 5:
+		
+		}
+	 } while (decision!=6);
+
+	}
 }
