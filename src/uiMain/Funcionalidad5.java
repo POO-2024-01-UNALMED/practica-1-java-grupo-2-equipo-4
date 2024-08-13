b17df1453d1f9f42a280ad800d7ef09fc9f26b23
Juanes6767 <159493494+Juanes6767@users.noreply.github.com>
2024-08-13 13:16:19 -0500

    Funcionalidad5 #1 (#30)
    * Delete .txt innecesario
    
    * Delete .txt
    
    * Delete .txt
    
    * Funcionalidad5.java #1

---
+++ src/uiMain/Funcionalidad5.java
@@ -0,0 +1,13 @@
+package uiMain;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import gestorAplicación.servicios.Tienda;
+import gestorAplicación.servicios.Enums.Categoria;
+
+import static uiMain.Main.print;
+import static uiMain.Main.escaner;
+
+public class Funcionalidad5 {
+	static Scanner sc = new Scanner(System.in);
+}
