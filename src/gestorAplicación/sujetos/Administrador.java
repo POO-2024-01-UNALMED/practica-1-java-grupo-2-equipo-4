4e25d673f00af592636cee5c130b5a03243c75ba
juandiegoordonezl <159456074+juandiegoordonezl@users.noreply.github.com>
2024-08-19 22:22:09 -0500

    algo

--- src/gestorAplicación/sujetos/Administrador.java
+++ src/gestorAplicación/sujetos/Administrador.java
@@ -1,11 +1,16 @@
 package gestorAplicación.sujetos;
 
+import java.util.ArrayList;
 import java.util.Map;
+
+import gestorAplicación.servicios.Tienda;
+
 import java.io.Serializable;
 
 public class Administrador extends Persona implements Serializable {
-	
 	private static final long serialVersionUID = -9139003303968753008L;
+	private ArrayList<Tienda> tiendas=new ArrayList<Tienda>();
+
 
 	@Override
 	public StringBuilder obtenerContadorProductos(Persona cliente) {
@@ -18,5 +23,15 @@ public class Administrador extends Persona implements Serializable {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	public ArrayList<Tienda> getTiendas() {
+		return tiendas;
+	}
+
+	public void setTiendas(ArrayList<Tienda> tiendas) {
+		this.tiendas = tiendas;
+	}
+	
+	
 
 }
