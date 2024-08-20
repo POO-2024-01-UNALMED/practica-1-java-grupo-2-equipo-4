1564c47eed5e88f663841e1d87bd8a96610e7421
Juanes6767 <jupinedahe@unal.edu.co>
2024-08-19 23:23:39 -0500

    Lineas temporales

--- src/baseDatos/Serializador.java
+++ src/baseDatos/Serializador.java
@@ -8,7 +8,11 @@ import java.util.ArrayList;
 import gestorAplicación.servicios.*;
 import gestorAplicación.servicios.Enums.Categoria;
 import gestorAplicación.servicios.Enums.Edades;
+import gestorAplicación.servicios.Enums.Genero;
 import gestorAplicación.servicios.Enums.Tamaño;
+import gestorAplicación.sujetos.Cajero;
+import gestorAplicación.sujetos.Conserje;
+import gestorAplicación.sujetos.Domiciliario;
 import gestorAplicación.sujetos.Persona;
 
 import java.io.IOException; 
@@ -38,43 +42,44 @@ public class Serializador {
 		serializar(Persona.getPersonas(),"clientes");
 		}
 	public static void main(String[] args){
-		Producto producto1 = new Producto(
-		            "Leche Entera", "La Vaquita", 1.50, Tamaño.GRANDE, Edades.MENORES, Categoria.ALIMENTO,
-		            "Leche entera de vaca, fortificada con vitaminas A y D.", "15/10/2024", 101
-		        );
-		Producto producto12 = new Producto(
-		            "Leche Entera", "La Vaquita", 1.50, Tamaño.GRANDE, Edades.MENORES, Categoria.ALIMENTO,
-		            "Leche entera de vaca, fortificada con vitaminas A y D.", "15/10/2024", 101
-		        );
-	        Producto producto13 = new Producto(
-		            "Leche Entera", "La Vaquita", 1.50, Tamaño.GRANDE, Edades.MENORES, Categoria.ALIMENTO,
-		            "Leche entera de vaca, fortificada con vitaminas A y D.", "15/10/2024", 101
-		        );
-
-	        Producto producto2 = new Producto(
-	            "Pan Integral", "PanRico", 2.00, Tamaño.MEDIANO, Edades.MENORES, Categoria.ALIMENTO,
-	            "Pan integral alto en fibra, ideal para una dieta saludable.", "22/08/2024", 102
-	        );
-
-	        Producto producto3 = new Producto(
-	            "Yogur Natural", "BioLácteos", 3.25, Tamaño.MEDIANO, Edades.ADULTOS, Categoria.ALIMENTO,
-	            "Yogur natural sin azúcar añadido, fuente de probióticos.", "10/09/2024", 103
-	        );
-
-	        Producto producto4 = new Producto(
-	            "Queso Cheddar", "La Gran Quesería", 4.50, Tamaño.MEDIANO, Edades.ADULTOS, Categoria.ALIMENTO,
-	            "Queso cheddar madurado, ideal para sandwiches y gratinados.", "30/09/2024", 104
-	        );
-
-	        Producto producto5 = new Producto(
-	            "Galletas de Avena", "Cerealia", 2.75, Tamaño.PEQUEÑO, Edades.MENORES, Categoria.ALIMENTO,
-	            "Galletas de avena y miel, perfectas para un snack saludable.", "05/11/2024", 105
-	        );
-	        ArrayList<Tienda> t=new ArrayList<>();
-	        ArrayList<Producto> entrega=new ArrayList<>();
-	     
-//		System.out.println(Proveedor.getSeisProveedores());
-//		serializar(Proveedor.getSeisProveedores(),"proveedores");
+//		Producto producto1 = new Producto(
+//		            "Leche Entera", "La Vaquita", 1.50, Tamaño.GRANDE, Edades.MENORES, Categoria.ALIMENTO,
+//		            "Leche entera de vaca, fortificada con vitaminas A y D.", "15/10/2024", 101
+//		        );
+//		Producto producto12 = new Producto(
+//		            "Leche Entera", "La Vaquita", 1.50, Tamaño.GRANDE, Edades.MENORES, Categoria.ALIMENTO,
+//		            "Leche entera de vaca, fortificada con vitaminas A y D.", "15/10/2024", 101
+//		        );
+//	        Producto producto13 = new Producto(
+//		            "Leche Entera", "La Vaquita", 1.50, Tamaño.GRANDE, Edades.MENORES, Categoria.ALIMENTO,
+//		            "Leche entera de vaca, fortificada con vitaminas A y D.", "15/10/2024", 101
+//		        );
+//
+//	        Producto producto2 = new Producto(
+//	            "Pan Integral", "PanRico", 2.00, Tamaño.MEDIANO, Edades.MENORES, Categoria.ALIMENTO,
+//	            "Pan integral alto en fibra, ideal para una dieta saludable.", "22/08/2024", 102
+//	        );
+//
+//	        Producto producto3 = new Producto(
+//	            "Yogur Natural", "BioLácteos", 3.25, Tamaño.MEDIANO, Edades.ADULTOS, Categoria.ALIMENTO,
+//	            "Yogur natural sin azúcar añadido, fuente de probióticos.", "10/09/2024", 103
+//	        );
+//
+//	        Producto producto4 = new Producto(
+//	            "Queso Cheddar", "La Gran Quesería", 4.50, Tamaño.MEDIANO, Edades.ADULTOS, Categoria.ALIMENTO,
+//	            "Queso cheddar madurado, ideal para sandwiches y gratinados.", "30/09/2024", 104
+//	        );
+//
+//	        Producto producto5 = new Producto(
+//	            "Galletas de Avena", "Cerealia", 2.75, Tamaño.PEQUEÑO, Edades.MENORES, Categoria.ALIMENTO,
+//	            "Galletas de avena y miel, perfectas para un snack saludable.", "05/11/2024", 105
+//	        );
+//	        ArrayList<Tienda> t=new ArrayList<>();
+//	        ArrayList<Producto> entrega=new ArrayList<>();
+	     new Domiciliario("Pablo",10101,20,Genero.H,100000,null,true,true,true,"moto");
+	     new Cajero("Carlos", 3030, 30, Genero.M, 25000, null, true, true);
+	     new Conserje("Sara",1313,21,Genero.M,100000,null,true,false);
+//		serializar(Tienda.getDesempleados(),"empleados");
 	}
 	
 	} 
